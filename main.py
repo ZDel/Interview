3bbb077862b38f463d97d986423eb3ef38b2aaa7
zdel <zachdelaney52@yahoo.com>
2025-08-21 13:40:57 -0400

    flask app up

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-import flask 
+from pathlib import Path
+
+from flask import (
+    Flask, request, redirect, url_for, send_from_directory,
+    render_template_string, abort, flash
+)
+BASE_DIR = Path(__file__).resolve().parent
+CSV_PATH = "Documents.csv"
+
+app = Flask(__name__)
+@app.route("/", methods=["GET"])
+def index():
+    return "Hello, World!"
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
